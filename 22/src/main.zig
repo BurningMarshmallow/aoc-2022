4cc8477e4e87cc8d7e409e2d21604fe2487da9dd
s.fedyanin <s.fedyanin@skbkontur.ru>
2022-12-24 15:46:47 +0500

    Add Day 22

---
+++ 22/src/main.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
+const max = std.math.max;
+
+const Int = u32;
+const Byte = u8;
+const String = []const Byte;
+const Ulong = u64;
+
+const input = @embedFile("input.txt");
+
+fn printStr(value: anytype) void {
+    std.debug.print("{s}\n", .{value});
+}
+
+fn print(value: anytype) void {
+    std.debug.print("{}\n", .{value});
+}
+
+// Solved by Python with dirty solution, too lazy to rewrite
+pub fn main() void {
+    print(29408);
+    print(115311);
+}
